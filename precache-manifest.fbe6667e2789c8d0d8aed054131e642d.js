c0ddcaf428647e0cd8657c9de266833f7a4f8631
Younes Elguerraoui <yoelguer@e3r13p3.1337.ma>
2022-07-24 19:55:11 +0100

    add

---
+++ precache-manifest.fbe6667e2789c8d0d8aed054131e642d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f08340422954d0c4e6b07a7d9b7de1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929e060c016fc311c65d",
+    "url": "/static/js/2.44901fd6.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.44901fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b92151f636a381791b",
+    "url": "/static/js/main.0bae0267.chunk.js"
+  },
+  {
+    "revision": "4cf20cacaed91da710b0",
+    "url": "/static/js/runtime-main.323e4788.js"
+  }
+]);
